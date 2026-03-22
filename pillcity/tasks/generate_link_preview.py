ef5d9aa94f89c97aa6e9e95edbfb03cff279e0bb
KTachibanaM <whj19931115@gmail.com>
2026-03-21 22:12:33 -0700

    use twitter url for link previewed from nitter

--- pillcity/tasks/generate_link_preview.py
+++ pillcity/tasks/generate_link_preview.py
@@ -1,6 +1,7 @@
 import os
 import urllib.parse
 import linkpreview
+from urllib.parse import unquote
 from mongoengine import connect, disconnect
 from pillcity.models import LinkPreview, LinkPreviewState
 from .celery import app, logger
@@ -30,6 +31,20 @@ def _get_nitter_url(url: str) -> str:
     return parsed_url.geturl()
 
 
+def _nitter_base_url() -> str:
+    return f"{'https' if os.environ['NITTER_HTTPS'] == 'true' else 'http'}://{os.environ['NITTER_HOST']}"
+
+
+def _get_nitter_logo_png_url() -> str:
+    return _nitter_base_url() + "/logo.png"
+
+
+def _get_twitter_media_cdn_url(nitter_url: str) -> str:
+    suffix = nitter_url.replace(f"{_nitter_base_url()}/pic/", '')
+    suffix = unquote(suffix)
+    return "https://pbs.twimg.com/" + suffix
+
+
 @app.task()
 def generate_link_preview(url: str):
     connect(host=os.environ['MONGODB_URI'])
@@ -37,18 +52,33 @@ def generate_link_preview(url: str):
     link_preview = LinkPreview.objects.get(url=url)  # type: LinkPreview
     try:
         processed_url = url
-        if _is_twitter(url):
+        is_twitter = _is_twitter(url)
+
+        if is_twitter:
             processed_url = _get_nitter_url(url)
 
         preview = linkpreview.link_preview(processed_url)
 
         link_preview.title = preview.title
         link_preview.subtitle = preview.description
+
         if preview.absolute_image:
-            link_preview.image_urls = [preview.absolute_image]
+            absolute_image = preview.absolute_image
+
+            if is_twitter:
+                if absolute_image == _get_nitter_logo_png_url():
+                    absolute_image = None
+                else:
+                    absolute_image = _get_twitter_media_cdn_url(absolute_image)
+
+            if absolute_image:
+                link_preview.image_urls = [absolute_image]
+
         link_preview.state = LinkPreviewState.Fetched
+
     except Exception as e:
         logger.warn(str(e))
         link_preview.state = LinkPreviewState.Errored
+
     link_preview.save()
     disconnect()
